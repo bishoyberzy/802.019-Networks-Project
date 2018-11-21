435ea84d22e57c00b825dce9aa2787d2b02020e8
markmmt <mark.mansy@gmail.com>
2018-11-21 22:47:56 +0200

    modified:   Assinment1/main.cpp

--- Assinment1/main.cpp
+++ Assinment1/main.cpp
@@ -11,7 +11,10 @@ int main()
     getline(cin,data);
     string transmitted_message = Generator(data,generator);
     string command;
+    while(1){
     getline(cin,command);
+    if(command.find("Quit")!=-1)
+        break;
     int token1=command.find("<");
     int token2=command.find("|");
     int token3=command.find("|",token2 + 1);
@@ -19,10 +22,13 @@ int main()
     string filename = command.substr(token1+1, token2 - token1-1);
     ofstream myfile (filename.c_str());
     myfile << "Transmitted Message:"<<endl<<transmitted_message <<endl;
+    cout<< "Transmitted Message:"<<endl<<transmitted_message <<endl;
+
 
     int alteri = command.find("alter",0);
     if (alteri==-1)
-    myfile << "verifier output:"<<endl<<verfier(transmitted_message, generator)<<endl;
+    {myfile << "verifier output:"<<endl<<(verfier(transmitted_message, generator)?"Message is Correct!":"Message is not Correct!")<<endl;
+	cout<< "verifier output:"<<endl<<(verfier(transmitted_message, generator)?"Message is Correct!":"Message is not Correct!")<<endl;}
     else
     {
         alteri+=5;
@@ -30,8 +36,12 @@ int main()
         int index = atoi(alter_arg.c_str());
         string altered_message = alter(transmitted_message,index-1);
         myfile<< "altered message:\n" << altered_message<<endl;
-        myfile << "verifier output:\n" << verfier(altered_message, generator);
-    }
+        cout<< "altered message:\n" << altered_message<<endl;
+        cout<< "crc: "<< binarydiv(generator, altered_message);
+        myfile << "verifier output:\n" << (verfier(altered_message, generator)?"Message is Correct!":"Message is not Correct!")<<endl;
+        cout<< "verifier output:\n" << (verfier(altered_message, generator)?"Message is Correct!":"Message is not Correct!")<<endl;
 
+    }
+    }
     return 0;
 }
