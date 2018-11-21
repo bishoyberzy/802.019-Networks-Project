1e839f9ea160aacd266e6738c80fecbe05992d1d
bishoyberzy <32980459+bishoyberzy@users.noreply.github.com>
2018-11-21 22:06:19 +0200

    Update BinaryDivision.cpp

--- BinaryDivision.cpp
+++ BinaryDivision.cpp
@@ -24,20 +24,17 @@ string binarydiv(string gx, string mx)
 	generatordegree = gx.length();
 	int l;			//iterator for mx
 	string zeros;		//divisor in case bit equalls 0
-	string zerosadded;      //zeros added equal to degree
+	
 
-	//parsing first rim from mx
+							//parsing first rim from mx
 	for (l = 0; l < generatordegree; l++)
 		rim += mx[l];
 
 	//zeros for xor
 	for (int i = 0; i < generatordegree; i++)
 		zeros += "0";
+	
 
-	// adding zeros equall to generator degree gx to message mx
-	for (int i = 0; i < generatordegree-1; i++)
-		zerosadded += "0";
-//	mx += zerosadded; //  message plus generator degree
 
 	while (l <= mx.length())		// stop when l reach end of msg
 	{
@@ -80,4 +77,5 @@ string binarydiv(string gx, string mx)
 	}
 	return rim;
 }
+
 
